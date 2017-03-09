a7fc444b716807bbc444659680b1f90c9755e0f7
Travis Koch <travis.koch@vizexplorer.com>
2017-03-09 16:13:09 +1300

    Removed try catch in main, allowing to throw.

--- readonly/src/main/java/com/vizexplorer/eval/App.java
+++ readonly/src/main/java/com/vizexplorer/eval/App.java
@@ -39,15 +39,8 @@ public class App
   public static void main( String[] args ) throws ParseException
   {
     Date bd = null;
-    try
-    {
-      bd = new SimpleDateFormat("yyyyMMdd").parse(args[3]);
-      Person p = new Person(args[1], args[2], bd);
-      System.out.println( "Person instance created: "+p);
-    } catch (ParseException e)
-    {
-      e.printStackTrace();
-      throw e;
-    }
+    bd = new SimpleDateFormat("yyyyMMdd").parse(args[3]);
+    Person p = new Person(args[1], args[2], bd);
+    System.out.println( "Person instance created: "+p);
   }
 }
